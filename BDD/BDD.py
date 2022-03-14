250ee88e43aee470effccf2f847cd23b13a0d03c
EDUARDO RODRIGUES DOS SANTOS <eduardorodrigues1599@gmail.com>
2022-03-14 20:14:21 -0300

    Create BDD.py

---
+++ BDD/BDD.py
@@ -0,0 +1,23 @@
+import os
+
+def clean():
+    os.system('cls')
+
+
+def inicio():
+    print('='*70)
+    print('Logica de Banco de Dados!')
+    print('='*70)
+
+
+strings = 'sei la'
+strings.upper
+while True:
+    clean()
+    inicio()
+    print(strings)
+
+    fechar = input()
+    if fechar == '':
+        
+        break
